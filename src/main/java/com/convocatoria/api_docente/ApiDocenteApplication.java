0ff96c7e0231458ec4d6c08ea6e502e845fe3192
SaberY24 <saberstyle2442@gmail.com>
2025-06-20 08:13:03 -0500

    Start

---
+++ src/main/java/com/convocatoria/api_docente/ApiDocenteApplication.java
@@ -0,0 +1,13 @@
+package com.convocatoria.api_docente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDocenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/convocatoria/api_docente/ApiDocenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convocatoria.api_docente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
